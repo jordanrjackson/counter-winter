2979e9a627876b77f3962eb9296b3daadc353c17
Jordan Jackson <jordanrjackson0275@gmail.com>
2019-01-11 14:21:21 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
+class App extends React.Component{
+  state= {value: 0}
+
+  render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h1 className= "App-title">Counter</h1>
+      <p>{this.state.value}</p>
+      <button>-</button>
+      <button>+</button>
       </div>
-    );
+    )
   }
 }
 
